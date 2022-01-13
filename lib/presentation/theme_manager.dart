d866d9a9bf90df148bae22a8bb9fe013b8c9fd91
Solano-Furlan <solanofurlan@outlook.com>
2022-01-13 21:19:33 +0000

    App bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_app/presentation/color_manager.dart';
+import 'package:mvvm_app/presentation/font_manager.dart';
+import 'package:mvvm_app/presentation/styles_manager.dart';
 import 'package:mvvm_app/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
+    // main colors
     primaryColor: ColorManager.primary,
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.primaryOpacity70,
     // card view theme
     cardTheme: CardTheme(
       color: ColorManager.white,
@@ -16,7 +19,16 @@ ThemeData getApplicationTheme() {
       elevation: AppSize.s4,
     ),
     // App bar theme
-
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
     // Button theme
 
     // Text theme
