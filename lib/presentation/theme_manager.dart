6905c4e61ed255062384b4d64c7eb6eba17f9477
Solano-Furlan <solanofurlan@outlook.com>
2022-01-13 21:12:21 +0000

    Card theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_app/presentation/color_manager.dart';
+import 'package:mvvm_app/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -9,7 +10,11 @@ ThemeData getApplicationTheme() {
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
     // card view theme
-
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
     // App bar theme
 
     // Button theme
